df3c6938421abf84143bb6de4aa7082d90e8987a
Filip Agh <filip11agh@gmail.com>
2020-10-05 13:27:12 +0200

    fix path

--- src/FileController/fileController.py
+++ src/FileController/fileController.py
@@ -1,17 +1,19 @@
 import io
+import os
+
 
 def loadLoginMsg():
-    f = io.open("./Data/txtFiles/welcomeMsg.txt", mode="r", encoding="utf-8")
+    f = io.open(os.path.abspath("Data/txtFiles/welcomeMsg.txt"), mode="r", encoding="utf-8")
     return f.read()
 
 def loadGamesMsg():
-    f = io.open("./Data/txtFiles/gameMsg.txt", mode="r", encoding="utf-8")
+    f = io.open(os.path.abspath("Data/txtFiles/gameMsg.txt"), mode="r", encoding="utf-8")
     return f.read()
 
 def loadCommands():
-    f = io.open("./Data/txtFiles/commands.txt", mode="r", encoding="utf-8")
+    f = io.open(os.path.abspath("Data/txtFiles/commands.txt"), mode="r", encoding="utf-8")
     return f.read()
 
 def loadBotInfo():
-    f = io.open("./Data/txtFiles/botinfo.txt", mode="r", encoding="utf-8")
+    f = io.open(os.path.abspath("Data/txtFiles/botinfo.txt"), mode="r", encoding="utf-8")
     return f.read()
